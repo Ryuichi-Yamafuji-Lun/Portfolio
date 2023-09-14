4e30183eddf131501a64654e5909552a98caecc7
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-09-15 03:46:02 +0900

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,15 @@
 
 const Contact = () => {
   return (
-    <div name="contact" className="w-full h-screen">
-      Contact
+    <div name="contact" className="grid w-full font-lato place-content-center">
+      <div className="container mx-auto p-10">
+        <div className="text-center text-line-white font-bold p-4">
+          <p className="text-6xl">Contact Me</p>
+        </div>
+        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 ">
+          contact
+        </div>
+      </div>
     </div>
   );
 };
