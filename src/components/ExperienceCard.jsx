7bb8382be83a3f0d3181bc6fffe80a01e92ffbf8
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-10-05 13:09:20 +0900

    Create ExperienceCard.jsx

---
+++ src/components/ExperienceCard.jsx
@@ -0,0 +1,8 @@
+
+export const ExperienceCard = ({ title, date, technologies, description, websiteLink}) => {
+  return (
+    <div className="transition w-full md:w-[500px] m-3 mt-20 md:mt-0 rounded-lg overflow-hidden hover:shadow-lg hover:backdrop-blur-md flex flex-col md:flex-row">
+        ExperienceCard
+    </div>
+  )
+}
