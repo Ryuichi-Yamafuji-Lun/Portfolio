75ce3e485996499e194c89583cdee6f55980e315
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-10-08 15:30:44 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // Light Mode
-        'light-red': '#FFAAAA',
-        'light-gold': '#EDDDB8',
-        'light-blue': '#AAAAFF',
-
-        // Dark Mode
-        'dark-red': '#AA0000',
-        'dark-gold': '#888800',
-        'dark-blue': '#0000AA',
-
         // Background Colors & Text
         'background-white': '#F2F2F2',
         'line-white': '#B0925A',
