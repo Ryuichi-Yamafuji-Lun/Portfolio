07d9e1d062ea5947eb93375279d9fa5e737e797d
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-10-08 16:57:53 +0900

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
+
+import { FaGithub, FaLinkedin, FaFileAlt } from "react-icons/fa";
 
 const MenuItems = [
   { label: "ABOUT ME", to: "about" },
@@ -9,11 +11,24 @@ const MenuItems = [
 
 const Home = () => {
   const [activeSection, setActiveSection] = useState(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768); 
 
   const handleSetActive = (to) => {
     const activeIndex = MenuItems.findIndex((item) => item.to === to);
     setActiveSection(activeIndex);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Update isMobile state on window resize
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div name="home" className="w-full h-screen font-lato">
@@ -24,33 +39,78 @@ const Home = () => {
         <p className="text-3xl text-line-white sm:text-4xl font-bold sm:text-left">
           software engineer.
         </p>
-        <ul className="flex flex-col p-10">
-          {MenuItems.map((item, index) => (
-            <li key={index} className="menu-item group">
-              <Link
-                to={item.to}
-                spy={true}
-                smooth={true}
-                duration={500}
-                onSetActive={handleSetActive}
-                offset={-100} // Adjust this offset as needed
-              >
-                <div className={`flex items-center ${activeSection === index ? "font-bold" : ""}`}>
-                  <span
-                    className="nav-indicator mr-4 h-px w-8 bg-black transition-all group-hover:w-16 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
-                    style={{ width: activeSection === index ? "75px" : "30px" }}
-                  ></span>
-                  <p
-                    className="transition-transform transform hover:scale-110 hover:translate-x-1"
-                    style={{ minWidth: "150px" }}
-                  >
-                    {item.label}
-                  </p>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
+
+        {/* Menu Items */}
+        {!isMobile && (
+          <ul className="flex flex-col pt-10">
+            {MenuItems.map((item, index) => (
+              <li key={index} className="menu-item group">
+                <Link
+                  to={item.to}
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  onSetActive={handleSetActive}
+                  offset={-100} 
+                >
+                  <div className={`flex items-center ${activeSection === index ? "font-bold" : ""}`}>
+                    <span
+                      className="nav-indicator mr-4 h-px w-8 bg-black transition-all group-hover:w-16 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+                      style={{ width: activeSection === index ? "75px" : "30px" }}
+                    ></span>
+                    <p
+                      className="transition-transform transform hover:scale-110 hover:translate-x-1"
+                      style={{ minWidth: "150px" }}
+                    >
+                      {item.label}
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+
+        {/* Footer Section */}
+        {!isMobile && (
+          <footer className="mt-10">
+            <ul className="flex space-x-4">
+              <li>
+                <a
+                  href="https://github.com/Ryuichi-Yamafuji-Lun"
+                  className="text-black flex items-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub />
+                  <span className="ml-2">Github</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/ryulun/"
+                  className="text-black flex items-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin />
+                  <span className="ml-2">Linkedin</span>
+                </a>
+              </li>
+              <li>
+                <Link
+                  to="resume"
+                  className="text-black flex items-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaFileAlt />
+                  <span className="ml-2">Résumé</span>
+                </Link>
+              </li>
+            </ul>
+          </footer>
+        )}
       </div>
     </div>
   );
